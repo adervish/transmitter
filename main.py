9bdaae97e579dde3fc444151654a32f14bcb95c9
Alex Derbes <xor@bentboolean.com>
2020-11-01 20:04:04 -0500

    working

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-def on_button_pressed_a():
-    radio.send_number(1)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_ab():
-    radio.send_number(0)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
 Command = ""
 Pin4 = 0
 Pin3 = 0
 Pin0 = 0
 radio.set_group(1)
+# basic.clear_screen()
+# led.plot(Pin3 / 250, Pin0 / 250)
 
 def on_forever():
     global Pin0, Pin3, Pin4, Command
     while True:
-        Pin0 = pins.analog_read_pin(AnalogPin.P0)
-        Pin3 = pins.analog_read_pin(AnalogPin.P3)
-        Pin4 = pins.analog_read_pin(AnalogPin.P4)
-        Command = "" + convert_to_text(Pin0) + "" + convert_to_text(Pin3) + "" + convert_to_text(Pin4)
+        Pin0 = pins.analog_read_pin(AnalogPin.P0) 
+        Pin3 = pins.analog_read_pin(AnalogPin.P3) 
+        Pin4 = pins.analog_read_pin(AnalogPin.P4) 
+        Command = convert_to_text(Pin0) + "," + convert_to_text(Pin3)
         radio.send_string(Command)
-        led.plot(Pin3 / 250, Pin4 / 250)
+        #basic.show_string(Command)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,14 +3,16 @@ let Pin4 = 0
 let Pin3 = 0
 let Pin0 = 0
 radio.setGroup(1)
-basic.forever(function () {
+//  basic.clear_screen()
+//  led.plot(Pin3 / 250, Pin0 / 250)
+// basic.show_string(Command)
+basic.forever(function on_forever() {
+    
     while (true) {
         Pin0 = pins.analogReadPin(AnalogPin.P0)
         Pin3 = pins.analogReadPin(AnalogPin.P3)
         Pin4 = pins.analogReadPin(AnalogPin.P4)
-        Command = "" + convertToText(Pin0) + "," + convertToText(Pin3) + "," + convertToText(Pin4)
+        Command = convertToText(Pin0) + "," + convertToText(Pin3)
         radio.sendString(Command)
-        basic.clearScreen()
-        led.plot(Pin3 / 250, Pin0 / 250)
     }
 })
